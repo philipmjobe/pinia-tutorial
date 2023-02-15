e6e1c14c794add1bf5034814388f18e3d070e3c5
Philip <philipmjobe@gmail.com>
2023-02-15 14:55:04 -0500

    creates post componenet

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,18 +1,27 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import { storeToRefs } from "pinia";
+import {useAuthorStore} from "../stores/author"
 import { usePostStore } from "../stores/post";
+import Post from "../components/Post.vue"
 
+const route = useRoute()
+const {getPostAuthor} = storeToRefs(useAuthorStore())
+const {fetchAuthors} = useAuthorStore()
 const { posts, loading, error } = storeToRefs(usePostStore());
 const { fetchPosts } = usePostStore();
 
-fetchPosts();
+fetchAuthors()
+fetchPosts(route.params.id);
 </script>
 
 <template>
   <main>
     <p v-if="loading">Loading posts...</p>
     <p v-if="error">{{error.message}}</p>
+    <p v-if="post">
+      <post :post="post" :author-"getAuthor"></post>
+    </p>
     // eslint-disable-next-line vue/no-use-v-if-with-v-for
     <p v-if="posts" v-for="post in posts" :key="post.id">
       <RouterLink :to="`/post/${post.id}`">{{post.title}}</RouterLink>
